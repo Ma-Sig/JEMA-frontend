c1cd5ab7df061afe2aa3b942b5ea50f856028ef1
Ma-Sig <122039527+Ma-Sig@users.noreply.github.com>
2025-05-16 18:01:54 -0500

    info-reports-v1

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,8 +1,12 @@
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
+import { InfoPageComponent } from "../info-page/info-page.component";
+import { ReportsComponent } from "../reports/reports.component";
 import { Route, Routes } from "@angular/router";
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'info', component: InfoPageComponent},
+    { path: 'reports', component: ReportsComponent},
 ]
--- src/app/shared/right-q/right-q.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-right-q',
-  templateUrl: './right-q.component.html',
-  styleUrls: ['./right-q.component.css'],
-  standalone: true,
-})
-export class RightQComponent {}
