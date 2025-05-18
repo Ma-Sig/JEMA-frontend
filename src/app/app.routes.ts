58c9cb33725bb1d63feefd8e4d0ad09dd447e74a
Emmanuel Vintimilla <emmanuel.vintimilla@ucuenca.edu.ec>
2025-05-17 20:22:45 -0500

    Dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
+import { routes as dashboard_routes } from './features/dashboard/dashboard.routes';
 import { routes as auth_routes } from './features/auth/auth.routes';
 import { routes as inventory_routes } from './features/inventory/inventory.routes';
 import { MashupComponent } from './features/mashup/mashup.component';
 import { LoansComponent } from './features/loans/loans.component';
 
 export const routes: Routes = [
+  ...dashboard_routes,
   ...auth_routes,
   ...inventory_routes.map((route) => ({
     ...route,
     path: `inventory/${route.path}`,
   })),
-  { path: 'mushup', component: MashupComponent },
+  { path: 'mashup', component: MashupComponent },
   { path: 'loans', component: LoansComponent }
 ];
