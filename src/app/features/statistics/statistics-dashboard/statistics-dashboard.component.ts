ef72c6f0acbe754d490e458f38eb2a411a8d77b9
JonnathanCuzco <jonnathan.cuzcot@ucuenca.edu.ec>
2025-05-19 04:35:08 -0500

    Stadistics primeros controles

--- src/app/features/statistics/statistics-dashboard/statistics-dashboard.component.ts
+++ src/app/features/statistics/statistics-dashboard/statistics-dashboard.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarChartComponent } from '../bar-chart/bar-chart.component';
 import { LineChartComponent } from '../line-chart/line-chart.component';
+import { DropdownComponent } from '../../../shared/dropdown/dropdown.component';
+import { CheckListComponent } from '../../../shared/check-list/check-list.component';
 
 @Component({
   selector: 'app-statistics-dashboard',
   standalone: true,
-  imports: [CommonModule, BarChartComponent, LineChartComponent],
+  imports: [
+    CommonModule,
+    BarChartComponent,
+    LineChartComponent,
+    DropdownComponent,
+    CheckListComponent,
+  ],
   templateUrl: './statistics-dashboard.component.html',
   styleUrl: './statistics-dashboard.component.css',
 })
-export class StatisticsDashboardComponent {}
+export class StatisticsDashboardComponent implements OnInit {
+  selectedLocation = '';
+  locations: string[] = [];
+  selectedItem = '';
+  items: string[] = [];
+
+  ngOnInit(): void {
+    this.loadSystemData();
+  }
+
+  loadSystemData() {
+    this.locations.push('Aula 1');
+    this.locations.push('Aula 2');
+    this.locations.push('Aula 3');
+
+    this.items.push('Computadora');
+    this.items.push('Proyector');
+    this.items.push('Silla');
+  }
+
+  onLocationChange(value: string) {
+    console.log('Estado seleccionado:', value);
+    this.selectedLocation = value;
+  }
+
+  onItemChange(value: string) {
+    console.log('Tipo seleccionado:', value);
+    this.selectedItem = value;
+  }
+}
